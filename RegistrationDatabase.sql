079e9f517a8b89ff3ba0ce893a6da9c00ab925e1
Piyush Sanjay Koli <piyushkoli77@gmail.com>
2021-03-10 16:42:11 +0530

    Update RegistrationDatabase.sql

--- RegistrationDatabase.sql
+++ RegistrationDatabase.sql
@@ -1,7 +1,23 @@
-CREATE DATABASE IF NOT EXISTS `registration_db`;
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.7.26-log - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.2.0.6213
+-- --------------------------------------------------------
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for registration_db
+CREATE DATABASE IF NOT EXISTS `registration_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `registration_db`;
 
+-- Dumping structure for table registration_db.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` varchar(255) DEFAULT NULL,
@@ -14,5 +30,13 @@ CREATE TABLE IF NOT EXISTS `users` (
   `designation` varchar(255) DEFAULT NULL,
   `joining_date` date DEFAULT NULL,
   `pincode` int(11) DEFAULT NULL,
+  `password` longtext,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
